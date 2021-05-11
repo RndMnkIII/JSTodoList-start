c47345cec8c7d507594e7d1e453a68ae0d184869
RndMnkIII <rndmnkiii@gmail.com>
2021-05-11 18:43:08 +0000

    02. Added function addTodo (alert)

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const btn = document.getElementById('add');
-btn.addEventListener('click', function () {
-    console.log('Click!');
-})
+document.addEventListener('DOMContentLoaded', function (){
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const table = document.getElementById('table');
+    const alert = document.getElementById('alert');
+    const btn = document.getElementById('add');
+    btn.onclick = addTodo;
+
+    function addTodo(){
+        if(title.value === '' || description.value === '') {
+            alert.classList.remove('d-none');
+            alert.innerText = 'Se requiere un titulo y descripción';
+            return;
+        }
+        alert.classList.add('d-none');
+     }
+});
