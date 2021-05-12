fd0804f18fd6a6d8b5d301fbb48b26182a13d760
RndMnkIII <rndmnkiii@gmail.com>
2021-05-12 21:11:33 +0000

    05. model.js: removeTodo(id)

---
+++ components/add-todo.js
@@ -0,0 +1,20 @@
+export default class AddTodo {
+    constructor(){
+        this.btn = document.getElementById('add');
+        this.title = document.getElementById('title');
+        this.description = document.getElementById('description');
+    }
+
+    onClick(callback){
+        this.btn.onclick = () => {
+            if(title.value === '' || description.value === '') {
+                // alert.classList.remove('d-none');
+                // alert.innerText = 'Se requiere un titulo y descripción';
+                // return;
+                console.error('Incorrecto');
+            } else {
+                callback(this.title.value, this.description.value);
+            }
+        }
+    }
+}
--- js/model.js
+++ js/model.js
@@ -2,6 +2,7 @@ export default class Model {
     constructor(){
         this.view = null;
         this.todos = [];
+        this.currentId = 1;
     }
 
     setView(view){
@@ -13,7 +14,22 @@ export default class Model {
     }
 
     addTodo(title, description){
-        console.log(title, description);
-        
+        const todo = {
+            id: this.currentId++,
+            title,
+            description,
+            completed: false,
+        }
+
+        this.todos.push(todo);
+        //console.log(todo);
+        //return Object.assign({},todo ); //asigna una copia del objeto todo para que no pueda ser modificado de forma externa a la clase
+        return {...todo}; //nos expande las propiedades de todo en un nuevo objeto. Sintaxis Spread
+        //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+    }
+
+    removeTodo(id){
+        const index = this.todos.findIndex((todo) => todo.id === id);
+        this.todos.splice(index, 1); //borra 1 elemento de todos a partir de la posicion index
     }
 }
